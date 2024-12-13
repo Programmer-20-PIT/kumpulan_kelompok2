d7f9569a8ccb646d09bec72510e0f460d3628097
Lily-lunaria <elizabeth.ly980@gmail.com>
2024-12-13 15:59:26 +0700

    aja

--- tugas_visibility.php
+++ tugas_visibility.php
@@ -18,22 +18,41 @@ class student{
 $sopo1 = new student();
 $sopo1->getName("anjay");
 
+echo"<br>";
 
+class mobil {
+    public $nissan;
+    protected $mustang;
+    private $bmw;
 
+    function mintamobil($nissan): mixed {
+        return $this->nissan = $nissan;
+    }
+    protected function mintamobil1($mustang) {
+        return $this->mustang = $mustang;
+    }
+    private function mintamobil2 ($bmw) {
+        return $this->bmw = $bmw;
+    }
+    public function getmobil2() {
+        return $this->mintamobil2("bmw");
+    }
+}
 
+$car1 = new mobil();
+$car2 = new mobil();
+echo $car1->mintamobil("Nissan");
+echo $car2->getmobil2();
+echo "<br>";
 
+class mobil2 extends mobil {
+    public function getMobil() {
+        return $this->mintamobil1("mustang");
+    }
+}
 
-
-
-
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
+$car2 = new mobil2();
+echo $car2->getmobil();
 
 
 ?>
